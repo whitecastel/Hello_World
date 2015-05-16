d99eaa436ee60c9f51e10771dd62a0c166ee9aae
realtek_malone <malone_ma@realtek.com>
2015-05-16 22:26:11 +0800

    a test

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,6 +3,6 @@ void main()
 {
 	printf("Hello world !\n");
 	printf("my name is hehui_zou\n");
-	//printf("my number is 1122\n");
-	printf("who are you?");
+	//printf("my number is 1122\n");i
+	printf("this ia a patch\n");
 }
